917dc67a0a4dd2283c6d04a775fd01e0e0a7e196
gustavo96033 <gustavo96033@gmail.com>
2026-04-09 14:09:59 -0300

    Update MateriaisPage.jsx

--- src/pages/MateriaisPage.jsx
+++ src/pages/MateriaisPage.jsx
@@ -1,1 +1,12 @@
+import { useAuth } from "../hooks/useAuth";
 
+export default function InventarioPage() {
+  const user = useAuth();
+
+  return (
+    <div>
+      <h2>Inventário</h2>
+      {user ? <p>Usuário logado: {user.email}</p> : <p>Faça login</p>}
+    </div>
+  );
+}
