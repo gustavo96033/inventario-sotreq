afe34a41740ad30325e5f60b66eff5a55df94476
gustavo96033 <gustavo96033@gmail.com>
2026-04-09 14:16:09 -0300

    Update InventarioPage.jsx

--- src/pages/InventarioPage.jsx
+++ src/pages/InventarioPage.jsx
@@ -1,12 +1,3 @@
-mport { useAuth } from "../hooks/useAuth";
-
 export default function InventarioPage() {
-  const user = useAuth();
-
-  return (
-    <div>
-      <h2>Inventário</h2>
-      {user ? <p>Usuário logado: {user.email}</p> : <p>Faça login</p>}
-    </div>
-  );
+  return <div>Inventário funcionando</div>;
 }
